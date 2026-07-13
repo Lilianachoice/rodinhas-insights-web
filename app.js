4735afa797d493d1386899bedc2f5e349c9297b1
Lilianachoice <liliana.braga@choicecar.pt>
2026-07-13 11:38:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,72 +16,6 @@ console.log("Leaflet:", typeof L);
 document.getElementById("ultimaAtualizacao").innerHTML =
   new Date().toLocaleString("pt-PT");
 
-// ==========================================
-// MAPA
-// ==========================================
-
-function iniciarMapa() {
-
-  mapa = L.map("map").setView([39.6, -8.0], 7);
-
-  L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-      maxZoom: 19,
-      attribution: "© OpenStreetMap"
-    }
-  ).addTo(mapa);
-
-  marcadores = L.layerGroup().addTo(mapa);
-
-}
-
-// ==========================================
-// Desenhar pedidos
-// ==========================================
-
-function desenharPedidos() {
-
-  marcadores.clearLayers();
-
-  pedidos.forEach(p => {
-
-    if (!p["Pickup Lat"])
-      return;
-
-    const cor =
-      p["Transport Type"] === "Shared"
-        ? "#F4C400"
-        : "#333333";
-
-    L.circleMarker(
-    [
-        Number(p["Pickup Lat"]),
-        Number(p["Pickup Lng"])
-    ],
-    {
-        radius: 7,
-        color: cor,
-        fillColor: cor,
-        fillOpacity: 0.9
-    }
-)
-.bindPopup(
-`
-<b>${p["Pickup Cidade"] || "Sem cidade"}</b><br>
-
-💶 ${(Number(p["Monthly Fee"]) || 0).toLocaleString("pt-PT")} €<br>
-
-👥 ${p["Transport Type"]}<br>
-
-📅 ${p["Dias"]}
-`
-)
-.addTo(marcadores);
-
-  });
-
-}
 
 // ==========================================
 // Carregar pedidos
@@ -99,7 +33,7 @@ async function carregarPedidos() {
 
     atualizarDashboard();
 
-    desenharPedidos();
+    desenharPedidos(pedidos);
 
   } catch (erro) {
 
