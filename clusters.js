9f677de1ce6d964f184ecdf4b8c6ceefe30eef2d
Lilianachoice <liliana.braga@choicecar.pt>
2026-07-13 12:15:49 +0100

    Update clusters.js

--- clusters.js
+++ clusters.js
@@ -1,1 +1,13 @@
+// ==========================================
+// CLUSTERS
+// ==========================================
 
+function obterPedidosFiltrados(listaPedidos) {
+
+    // Para já devolvemos todos.
+    // Nos próximos passos vamos aplicar os filtros
+    // da sidebar aqui.
+
+    return listaPedidos;
+
+}
